45a2c9c94c3a3b93e136dad6c2e11e120e60855b
Uwe L. Korn <uwe.korn@quantco.com>
2025-10-27 15:53:50 +0100

    PYO3_USE_ABI3_FORWARD_COMPATIBILITY=1

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,6 +5,10 @@ set -ex
 export BINDGEN_EXTRA_CLANG_ARGS="$CFLAGS"
 export LIBCLANG_PATH=$BUILD_PREFIX/lib/libclang${SHLIB_EXT}
 
+if [[ "$PY_VER" == "3.14" ]]; then
+  export PYO3_USE_ABI3_FORWARD_COMPATIBILITY=1
+fi
+
 maturin build --release --strip --manylinux off --interpreter="${PYTHON}" -m Cargo.toml
 
 "${PYTHON}" -m pip install $SRC_DIR/target/wheels/*.whl --no-deps -vv
